b474e9f6cdd257bef84483f52da8c36417ffee69
kasa021 <mkafa.k11@gmail.com>
2023-03-15 20:04:16 +0900

    コメントを追加

--- backend/src/todo/todo.controller.ts
+++ backend/src/todo/todo.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 
-@Controller('todo')
+@Controller('todo') // このコントローラーのルートは /todo になる
 export class TodoController {
 
-  @Get("list")
+  @Get("list") //HTTPメソッドがGETで、パスが /todo/list になる
   getList() {
     return [
-      {
+      { // この配列の要素はJSONオブジェクトになる
+        //この内容がresponse bodyになる
         title: "牛乳を買いに行く",
         due_on: "2022-12-24",
         done: false

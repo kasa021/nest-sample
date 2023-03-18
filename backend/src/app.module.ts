c115ba6f6f5e53685e97449d93bec6e10bbcde74
kasa021 <mkafa.k11@gmail.com>
2023-03-18 18:34:08 +0900

    frontend,backendを初期化

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TodoController } from './todo/todo.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, TodoController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}

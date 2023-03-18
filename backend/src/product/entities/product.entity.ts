4f76c5881bc41ad2727db2645395d3760ddb1c8d
kasa021 <mkafa.k11@gmail.com>
2023-03-18 22:02:01 +0900

    Entityを作成

--- backend/src/product/entities/product.entity.ts
+++ backend/src/product/entities/product.entity.ts
@@ -1,1 +1,19 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  image: string;
+
+  @Column()
+  price: number;
+}

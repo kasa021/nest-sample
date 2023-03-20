c50790c8a75cf6ba55f90c0dcedc89fff9d0c2e6
kasa021 <mkafa.k11@gmail.com>
2023-03-20 19:59:27 +0900

    user.entity.tsの内容を修正（コピペ）

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,1 +1,37 @@
-export class User {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  Timestamp,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn({
+    name: 'id',
+    unsigned: true,
+    type: 'smallint',
+    comment: 'ID',
+  })
+  readonly id: number;
+
+  @Column('varchar', { comment: 'ユーザー名' })
+  username: string;
+
+  @Column('varchar', { name: 'hashed_password', comment: 'パスワードハッシュ' })
+  hashedPassword: string;
+
+  @CreateDateColumn({
+    name: 'create_at',
+    comment: '作成時刻',
+  })
+  readonly createdAt?: Timestamp;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    comment: '更新時刻',
+  })
+  readonly updatedAt?: Timestamp;
+}
